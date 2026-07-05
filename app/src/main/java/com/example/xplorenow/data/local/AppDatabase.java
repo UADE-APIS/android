2c750495feb9f63fa3446e9352b9102976cfb014
MarcoTrujillo10 <mtrujillo@uade.edu.ar>
2026-07-05 11:31:40 -0300

    Agrega indicador de novedades en favoritos

--- app/src/main/java/com/example/xplorenow/data/local/AppDatabase.java
+++ app/src/main/java/com/example/xplorenow/data/local/AppDatabase.java
@@ -3,7 +3,8 @@ package com.example.xplorenow.data.local;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {CachedBooking.class}, version = 3, exportSchema = false)
+@Database(entities = {CachedBooking.class, CachedFavorite.class}, version = 4, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CachedBookingDao cachedBookingDao();
+    public abstract CachedFavoriteDao cachedFavoriteDao();
 }
